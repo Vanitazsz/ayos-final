95ea8b613d9136075716469d39a758f4098802a2
Vanitazsz <ariaslhanzjoshua@gmail.com>
2026-07-23 01:05:50 +0800

    Add temp seed SQL

---
+++ ayos-try/temp_seed.sql
@@ -0,0 +1,1 @@
+SELECT provider, model, outcome, error_code, http_status, created_at FROM public.ai_analysis_attempts WHERE account_id='032a57ad-8355-4751-99f8-2b1b4cc8ebf9' ORDER BY created_at DESC LIMIT 5;
